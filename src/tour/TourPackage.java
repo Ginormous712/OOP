29fae8d9fb71b843b31df9cae6b0f8f09bac8441
Ginormous712 <romavitaliy470@gmail.com>
2023-11-15 00:35:36 +0200

    Finished all necessary classes and enums. Need to add unit-tests. Need to add user interface (user class may be).

--- src/tour/TourPackage.java
+++ src/tour/TourPackage.java
@@ -1,19 +1,20 @@
 package tour;
 public class TourPackage {
     private static int idCounter;
-    private int id;
-    private String[] countries;
-    private TourType type;
-    private int duration;
-    private String transport;
-    private String meals;
-    private double price;
+    private final int id;
+    private final String[] countries;
+    private final TourType type;
+    private final int duration;
+    private final TransportType transport;
+    private final MealType meals;
+    private final double price;
 
     static {
         idCounter = 1;
     }
 
-    public TourPackage(String[] countries, TourType type, int duration, String transport, String meals, double price) {
+    public TourPackage(String[] countries, TourType type, int duration,
+                       TransportType transport, MealType meals, double price) {
         this.id = idCounter++;
         this.countries = countries;
         this.type = type;
@@ -21,5 +22,45 @@ public class TourPackage {
         this.transport = transport;
         this.meals = meals;
         this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String[] getCountries() {
+        return countries;
+    }
+
+    public TourType getType() {
+        return type;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public TransportType getTransport() {
+        return transport;
+    }
+
+    public MealType getMeals() {
+        return meals;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Tour Package #%d\n" +
+                        "Countries: %s\n" +
+                        "Type: %s\n" +
+                        "Duration: %d days\n" +
+                        "Transport: %s\n" +
+                        "Meals: %s\n" +
+                        "Price: $%.2f",
+                id, String.join(", ", countries), type, duration, transport, meals, price);
     }
 }
