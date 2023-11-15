b56a17692627dcb82b33936c0ac89221c4280389
Ginormous712 <romavitaliy470@gmail.com>
2023-11-15 22:36:13 +0200

    Fixed bug with wrong number of initialized TourPackages. Still need to add UI and unit-tests.

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -5,12 +5,12 @@ import tour.*;
 
 public class Main {
     public static void main(String[] args) {
-        TourManager tourManager = new TourManager();
-
+        //TourManager tourManager = new TourManager();
+        /*
         for (int i = 0; i < 10; i++) {
             tourManager.addTourPackage(TourGenerator.generateRandomTour());
         }
-
+        */
         //tourManager.displayTourPackages();
         //tourManager.sortTourPackagesByPrice();
         //tourManager.sortTourPackagesByCountry();
